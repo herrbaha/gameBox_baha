f627f51e69119dbbbad20072c59887d8abcffdcd
bahadir <baha_i@hotmail.com>
2021-07-19 09:58:46 +0200

    if kondition für random erstelltfür click

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ random = Math.floor(Math.random() *16);
 console.log(random );
 squares.forEach((square, index) => {
  square.addEventListener('click', click);
+ if (index == random || index == random +2) {
+    square.addEventListener('click', function() {
+      this.style.backgroundColor = 'green';
+      alert('Congratulations');
 })
