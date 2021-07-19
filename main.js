2aa171d457a235a39ea2eaf190f009bab103743a
bahadir <baha_i@hotmail.com>
2021-07-19 14:42:26 +0200

    click function erstellt2

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ console.log(squares);
 random = Math.floor(Math.random() *16);
 console.log(random );
 squares.forEach((square, index) => {
- square.addEventListener('click', click);
- if (index == random || index == random +2) {
+  square.addEventListener('click', click);
+  
+  if (index == random || index == random +2) {
     square.addEventListener('click', function() {
       this.style.backgroundColor = 'green';
       alert('Congratulations');
