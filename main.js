f069eac6be3c562c6beb03d041f69f6d66516e92
bahadir <baha_i@hotmail.com>
2021-07-19 09:50:25 +0200

    addeventlistener erstelltfür click

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const squares = document.querySelectorAll('div');
 console.log(squares);
 random = Math.floor(Math.random() *16);
 console.log(random );
+squares.forEach((square, index) => {
+ square.addEventListener('click', click);
+})
