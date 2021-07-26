3297866be7f8aa87933a0716be9a0548b096f788
bahadir <baha_i@hotmail.com>
2021-07-26 12:57:59 +0200

    random auser funct  gebracht

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const squares = document.querySelectorAll('div');
-console.log(squares);
-
+//console.log(squares);
+random = Math.floor(Math.random() *16);
+//console.log(random );
 squares.forEach((square, index) => {
   square.addEventListener('click', click);
-  random = Math.floor(Math.random() *16);
-      console.log(random );
+ 
   if (index == random) {
     square.addEventListener('click', function() {
       
